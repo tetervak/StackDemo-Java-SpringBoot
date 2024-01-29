bfb46a5181674e30b9d0728e420284482e919b75
Alex Tetervak <tetervak@sheridancollege.ca>
2024-01-29 11:20:59 -0500

    converted to using record

--- src/main/java/ca/tetervak/stackdemo/model/StackItem.java
+++ src/main/java/ca/tetervak/stackdemo/model/StackItem.java
@@ -1,20 +1,4 @@
 package ca.tetervak.stackdemo.model;
 
-public class StackItem {
-
-    private final int count;
-    private final String value;
-
-    public int getCount() {
-        return count;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public StackItem(int count, String value) {
-        this.count = count;
-        this.value = value;
-    }
+public record StackItem(int count, String value) {
 }
