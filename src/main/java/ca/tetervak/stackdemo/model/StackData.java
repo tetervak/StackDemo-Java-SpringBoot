d20a1a25e70f4701b979171ca735e1a9a20cd0e1
Alex Tetervak <tetervak@sheridancollege.ca>
2024-01-29 16:51:39 -0500

    removed an unused import

--- src/main/java/ca/tetervak/stackdemo/model/StackData.java
+++ src/main/java/ca/tetervak/stackdemo/model/StackData.java
@@ -1,6 +1,5 @@
 package ca.tetervak.stackdemo.model;
 
-import ca.tetervak.stackdemo.controller.StackController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
